d1e976764aae4915985df1a7fb1e5a402116ec08
priyank savaliya <priyanksavaliya21@gmail.com>
2023-01-08 03:24:10 +0530

    first

---
+++ assets/js/custom.js
@@ -0,0 +1,16 @@
+$(window).resize(function() {
+    /*--------- Service Slider Js ----------*/
+
+});
+
+$(document).ready(function () {
+
+
+    //Menu Js
+    $('.menu_toggle_btn').click(function () {
+        $('.header_menu').toggleClass('open_menu');
+        $('body').toggleClass('cm-overflow');
+    });
+
+   
+});
